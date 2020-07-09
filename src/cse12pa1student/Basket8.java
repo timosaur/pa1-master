b2dd4f68b0940eac9ac84957f7c91f794612e86a
Timothy Wang <timzwang@gmail.com>
2020-07-08 19:01:36 -0700

    Explanation for Basket8.addToBasket

--- src/cse12pa1student/Basket8.java
+++ src/cse12pa1student/Basket8.java
@@ -42,9 +42,18 @@ public class Basket8 implements Basket {
 
     @Override
     public void addToBasket(Item i) {
+        // This is a list of lists. Each index has a list
+        // Index 1 is the list where the last new item added will be in
+
+        // Start with index 1. This basket never uses index 0
+        // For each index in outer list, search for item...
         for (int j = 1; j < this.items.size(); j++) {
+            // If item exists in sublist...
             if (this.items.get(j).contains(i)) {
+                // Remove item from sublist...
                 this.items.get(j).remove(i);
+                // Then reinsert item in the NEXT sublist after next item
+                // (Make a new sublist after if necessary)
                 if (j + 1 == this.items.size()) {
                     ArrayList<Item> temp = new ArrayList<Item>();
                     temp.add(i);
@@ -53,10 +62,15 @@ public class Basket8 implements Basket {
                     this.items.get(j + 1).add(i);
                 }
                 return;
+                // So basically, if an item is found, move it to the next sublist
+                // Maybe this is to show how many times an item has added (the index j is how many times)
             }
         }
+        // We reach this point only if item is brand new (has not been found)
+        // Make sure the list has a sublist at index 1
         while(this.items.size() <= 1)
             this.items.add(new ArrayList<Item>());
+        // Add item to beginning, if item is new. This indicates that item has count 1 (index 1)
         this.items.get(1).add(i);
     }
 
